bc72a5b055adbd91ce9f7695f72636e08d10abca
Clara Angelita Karunia Dewi <claraanjelitakd@gmail.com>
2026-07-30 03:09:16 +0700

    Fix baseURL, Database variables, and enable Development mode to debug Whoops error

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => '127.0.0.1',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
+        'hostname'     => getenv('MYSQLHOST') ?: getenv('MYSQL_HOST') ?: '127.0.0.1',
+        'username'     => getenv('MYSQLUSER') ?: getenv('MYSQL_USER') ?: '',
+        'password'     => getenv('MYSQLPASSWORD') ?: getenv('MYSQL_PASSWORD') ?: '',
+        'database'     => getenv('MYSQLDATABASE') ?: getenv('MYSQL_DATABASE') ?: '',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
@@ -41,7 +41,7 @@ class Database extends Config
         'compress'     => false,
         'strictOn'     => false,
         'failover'     => [],
-        'port'         => 3306,
+        'port'         => getenv('MYSQLPORT') ?: getenv('MYSQL_PORT') ?: 3306,
         'numberNative' => false,
         'foundRows'    => false,
         'dateFormat'   => [
