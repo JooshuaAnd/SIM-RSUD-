5b9bdf389067bfbb975a892ab6f558ecd6d2d236
Clara Angelita Karunia Dewi <claraanjelitakd@gmail.com>
2026-08-05 22:59:06 +0700

    Fix case sensitivity for custom_pagination view path

--- app/Config/Pager.php
+++ app/Config/Pager.php
@@ -24,7 +24,7 @@ class Pager extends BaseConfig
         'default_full'   => 'CodeIgniter\Pager\Views\default_full',
         'default_simple' => 'CodeIgniter\Pager\Views\default_simple',
         'default_head'   => 'CodeIgniter\Pager\Views\default_head',
-        'custom_pagination' => 'App\Views\pelatihan\layout\custom_pagination',
+        'custom_pagination' => 'App\Views\Pelatihan\layout\custom_pagination',
     ];
 
     /**
